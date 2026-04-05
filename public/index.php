4774a0eedabe17e416b33aa4c0e19b679597cc2d
MULO <muloproject@gmail.com>
2026-04-05 20:12:19 +0200

    feat: make callable routes handler

--- public/index.php
+++ public/index.php
@@ -1,2 +1,14 @@
 <?php
-echo $_SERVER['REQUEST_URI'];
+
+$routes = [
+    '/' => function() { echo "Home page"; },
+    '/about' => function() { echo "About page"; },
+    '/contact' => function() { echo "Contact page"; },
+];
+
+$url = $_SERVER['REQUEST_URI'];
+
+if (isset($routes[$url])) {
+    $handler = $routes[$url];
+    $handler();
+} else {echo "404";}
