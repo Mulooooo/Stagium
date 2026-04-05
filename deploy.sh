09bd7d412933c8cabe6302806e2abd07564807f3
MULO <muloproject@gmail.com>
2026-04-05 19:01:48 +0200

    first commit

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+DEST="/srv/http/stagium"
+USER="http"
+
+sudo rsync -a --delete --exclude='.git' ./ "$DEST/"
+
+sudo chown -R "$USER:$USER" "$DEST"
---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+sudo cp apache/stagium.conf /etc/httpd/conf/extra/httpd-vhosts.conf
