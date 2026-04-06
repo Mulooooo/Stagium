2a58052449d094a7859390800aa194037c76f18f
MULO <muloproject@gmail.com>
2026-04-06 23:13:37 +0200

    feat: extract router into separate class

--- public/index.php
+++ public/index.php
@@ -1,14 +1,11 @@
 <?php
 
-$routes = [
-    '/' => function() { echo "Home page"; },
-    '/about' => function() { echo "About page"; },
-    '/contact' => function() { echo "Contact page"; },
-];
+require_once __DIR__ . '/../src/Core/Router.php';
 
-$url = $_SERVER['REQUEST_URI'];
+$r = new Router();
 
-if (isset($routes[$url])) {
-    $handler = $routes[$url];
-    $handler();
-} else {echo "404";}
+$r->addRoute('/', function() { echo "Home page";});
+$r->addRoute('/about', function() { echo "About page";});
+$r->addRoute('/contact', function() { echo "Contact page";});
+
+$r->run();
---
+++ src/Core/Router.php
@@ -0,0 +1,17 @@
+<?php
+
+class Router{
+    private array $routes = [];
+
+    public function addRoute($route, $handler){
+        $this->routes[$route] = $handler;
+    }
+
+    public function run() {
+        $url = $_SERVER['REQUEST_URI'];
+        if (isset($this->routes[$url])) {
+            $handler = $this->routes[$url];
+            $handler();
+        } else {echo "404";}
+    }
+}
