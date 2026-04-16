9886b5eb26d6ba46bb62921fda4989c46f1914ea
MULO <muloproject@gmail.com>
2026-04-17 01:58:56 +0200

    feat: migrate router to class-based handlers with role protection

--- src/Core/Router.php
+++ src/Core/Router.php
@@ -4,15 +4,34 @@ namespace App\Core;
 class Router{
     private array $routes = [];
 
-    public function addRoute($route, $handler){
-        $this->routes[$route] = $handler;
+    public function addRoute($route, $handler, $roles = []){
+        $this->routes[$route] = [
+            'handler' => $handler,
+            'roles' => $roles
+        ];
     }
 
     public function run() {
         $url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
         if (isset($this->routes[$url])) {
-            $handler = $this->routes[$url];
-            $handler();
-        } else {echo "404";}
+            $route = $this->routes[$url];
+            $handler = $route['handler'];
+            $roles = $route['roles'];
+            if (!empty($roles)) {
+                if (!isset($_SESSION['user_id'])) {
+                    header('Location: /login');
+                    exit;
+                }
+                if (!in_array($_SESSION['user_role'], $roles)) {
+                    http_response_code(403);
+                    die("403 Forbidden");
+                }
+            }
+            $controller = new $handler[0]();
+            $controller->{$handler[1]}();
+        } else {
+            http_response_code(404);
+            die("404 Not Found");
+        }
     }
 }
