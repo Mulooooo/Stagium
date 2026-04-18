3f4e2e6e68e209ea535a5df66e339c95c4856705
MULO <muloproject@gmail.com>
2026-04-19 00:19:24 +0200

    feat: add offer create

--- src/Controllers/OfferController.php
+++ src/Controllers/OfferController.php
@@ -25,4 +25,40 @@ class OfferController extends Controller{
             $this->render("offers/show.html.twig", ['offer' => $offer]);
         }
     }
+    public function create(){
+        $offerModel = new OfferModel();
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $data = $_POST;
+            $offer = $offerModel->create($data);
+            header('Location: /offers');
+            exit;
+        }
+        $sites = $offerModel->getSites();
+        $this->render("offers/create.html.twig", ['sites' => $sites]);
+    }
+    public function edit(){
+        $id = $_GET['id'] ?? null;
+        if (!$id) { 
+            header('Location: /offers'); 
+            exit;
+        }
+        $comanyModel = new OfferModel();
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $data = $_POST;
+            $offer = $comanyModel->update($id, $data);
+            header('Location: /offers');
+            exit;
+        }
+        $offer = $comanyModel->findById($id);
+        $this->render("offers/edit.html.twig", ['offer' => $offer]);
+    }
+    public function delete(){
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $id = $_POST['id'];
+            $offerModel = new OfferModel();
+            $offers = $offerModel->delete($id);
+            header('Location: /offers');
+            exit;
+        }
+    }
 }
