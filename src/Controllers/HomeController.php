61070e205f994ff1fea0f42d8f92316f9edb8fb9
MULO <muloproject@gmail.com>
2026-04-24 18:20:28 +0200

    feat: improve home page layout

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -1,8 +1,11 @@
 <?php
 namespace App\Controllers;
+use App\Models\OfferModel;
 
 class HomeController extends Controller{
     public function index(){
-        $this->render("home.html.twig", ['title' => 'Hello World!']);
+        $offerModel = new OfferModel();
+        $offers = $offerModel->getActiveOffers(1, 3);
+        $this->render("home.html.twig", ['latest_offers' => $offers['items']]);
     }
 }
