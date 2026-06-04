c4b64084e4aa1a12e237e21c07a2cb4cdccfa9b2
MULO <muloproject@gmail.com>
2026-06-04 15:55:27 +0200

    fix: add some sesion and pdo security

--- src/Models/Database.php
+++ src/Models/Database.php
@@ -12,7 +12,7 @@ class Database {
         "mysql:host={$config['DB_HOST']};dbname={$config['DB_NAME']};charset=utf8mb4",
         "{$config['DB_USER']}",
         "{$config['DB_PASS']}",
-        [\PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC]
+        [\PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION]
         );
     }
 
